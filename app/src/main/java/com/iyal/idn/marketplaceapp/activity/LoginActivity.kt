7c0b01efc98a25ff327ef20382706ba7535e028b
iyaalfiryal <iyaalfiryal@gmail.com>
2020-10-06 10:33:37 +0700

    Initial commit

---
+++ app/src/main/java/com/iyal/idn/marketplaceapp/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.iyal.idn.marketplaceapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iyal.idn.marketplaceapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
